0f82a6324e2d7a44576fc84c1c06e8793f08aab2
xcrazybet <dilovantalan@gmail.com>
2025-12-24 18:32:03 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSy...", // Use environment variables during build process
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
